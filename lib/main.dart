552663da21e90881bab77b1ba399fbe56e1955e1
Malaga-Karl <kbkmalaga2021@plm.edu.ph>
2024-04-24 00:32:28 +0800

    created inital login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/login.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Placeholder(),
+      home: const LoginPage(),
     );
   }
 }

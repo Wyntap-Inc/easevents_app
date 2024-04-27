a47b21c1ec6738f8ad4bda9f3e2395b450446546
Malaga-Karl <kbkmalaga2021@plm.edu.ph>
2024-04-27 16:04:41 +0800

    updated login: texttheme styles, richtext, singlechild scrollview. updated main: portrait mode only

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -7,140 +7,148 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.only(left: 16, right: 16, top: 50),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: 60,
-                height: 60,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.primary,
-                  borderRadius: BorderRadius.circular(15),
-                ),
-              ),
-              const SizedBox(
-                height: 17,
-              ),
-              const Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Text('Welcome back, Please enter your details'),
-              const SizedBox(
-                height: 32,
-              ),
-              const Text(
-                'Email address',
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'username@gmail.com',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16, top: 50),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary,
+                    borderRadius: BorderRadius.circular(15),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              const Text(
-                'Password',
-                style: TextStyle(
-                  fontSize: 16,
+                const SizedBox(
+                  height: 24,
                 ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: '********',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
+                const Text('Welcome back, Please enter your details'),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text('Login',
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          fontWeight: FontWeight.bold,
+                        )),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Email address',
+                  style: Theme.of(context).textTheme.bodyMedium!,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'username@gmail.com',
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text('Forgot Password?'),
+                const SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  'Password',
+                  style: Theme.of(context).textTheme.bodyMedium!,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: '********',
                   ),
-                ],
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: FilledButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    RichText(
+                      textAlign: TextAlign.end,
+                      text: TextSpan(
+                        text: 'Forgot password?',
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                              color: AppColors.primaryColor,
+                              fontWeight: FontWeight.w600,
+                            ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => const LoginPage(),
+                              ),
+                            );
+                          },
                       ),
                     ),
-                  ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                ElevatedButton(
                   onPressed: () {},
                   child: const Text('Sign in'),
                 ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: OutlinedButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
+                const SizedBox(
+                  height: 12,
+                ),
+                OutlinedButton.icon(
+                  onPressed: () {},
+                  icon: Image.asset(
+                    'asset/icon/google.png',
+                    height: 24,
+                    width: 24,
+                  ),
+                  label: const Text('Sign in with Google'),
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(
+                      color: AppColors.enabledBorderSideColor,
                     ),
                   ),
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/images/GoogleLogo.png',
-                        width: 24,
-                      ),
-                      const SizedBox(
-                        width: 12,
-                      ),
-                      const Text('Sign in with Google'),
-                    ],
-                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Don\'t have an account?'),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text('Sign up'),
-                  ),
-                ],
-              ),
-            ],
+                const SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Don\'t have an account? ',
+                            style:
+                                Theme.of(context).textTheme.bodySmall!.copyWith(
+                                      color: AppColors.textPrimary,
+                                    ),
+                          ),
+                          TextSpan(
+                            text: 'Sign up',
+                            style:
+                                Theme.of(context).textTheme.bodySmall!.copyWith(
+                                      color: AppColors.primaryColor,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.of(context).pop();
+                              },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),

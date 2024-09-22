f919d3441350a2b631d740493d90c3cff11bb2a3
Erish Sounder Latorre <elatorre.dev@gmail.com>
2024-09-22 19:45:35 +0800

    continue vendor profile screen

--- lib/models/consumer_account.dart
+++ lib/models/consumer_account.dart
@@ -1,11 +1,9 @@
 class ConsumerAccount {
-  final String id;
   final String firstName;
   final String lastName;
   final String? profilePhoto;
 
   ConsumerAccount({
-    required this.id,
     required this.firstName,
     required this.lastName,
     this.profilePhoto,
@@ -13,14 +11,12 @@ class ConsumerAccount {
 
   factory ConsumerAccount.fromJson(Map<String, dynamic> json) =>
       ConsumerAccount(
-        id: json['_id'],
         firstName: json['firstName'],
         lastName: json['lastName'],
         profilePhoto: json['profilePhoto'],
       );
 
   Map<String, dynamic> toJson() => {
-        '_id': id,
         'firstName': firstName,
         'lastName': lastName,
         'profilePhoto': profilePhoto,

31e49defe655f5137340124576d8b44cf18eaf07
Erish Sounder Latorre <erishsounderlatorre@Erishs-MacBook-Pro.local>
2025-03-18 18:06:20 +0800

    fix dependencies

--- android/app/src/main/java/com/easevents/easevents_app/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.easevents.easevents_app;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-    // You can override methods if needed
-}
